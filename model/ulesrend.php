d7afc7a33bebdb0f0a9a6742eed465ef9d60178d
ArthabZerda <b.arthab1022@gmail.com>
2021-10-21 10:51:32 +0200

    phpinfo

---
+++ model/ulesrend.php
@@ -0,0 +1,34 @@
+<?php
+class ulesrend{
+    private $id;
+    private $nev;
+    private $sor;
+    private $oszlop;
+    private $jelszo;
+    private $felhasznalo;
+
+    public function set_user($id, $conn){
+        //adatbázisban lekérdzeés
+
+            $sql ="SELECT id, nev, sor, oszlop, jelszo, felhasznalo FROM ulesrend";
+            $sql .= "WHERE id = $id";
+            $resul = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $this->id $row['id'];
+                $this->nev $row['nev'];
+                $this->sor $row['sor'];
+                $this->oszlop $row['oszlop'];
+                $this->jelszo $row['jelszo'];
+                $this->felhasznalo $row['felhasznalo'];
+                
+                } 
+    }
+    public function get_usernev(){
+        
+               return $this->$nev;
+                
+    }
+}
+?>
