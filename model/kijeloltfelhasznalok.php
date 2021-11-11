ba8c32d4371ce3ed4aabe3cd28d5cae084930dd3
ArthabZerda <b.arthab1022@gmail.com>
2021-11-11 09:22:25 +0100

    kf

---
+++ model/kijeloltfelhasznalok.php
@@ -0,0 +1,64 @@
+<?php
+
+class kijeloltfelhasznalok{
+    private $id;
+    protected $tablanev
+
+    protected function set_id($id, $conn){
+        //adatbázisban lekérdzeés
+
+            $sql ="SELECT id FROM $this->$tablanev WHERE id = $id";
+            $result = $conn->query($sql);
+            
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $this->id = $row['id'];
+                }
+                else{
+                    $sql ="INSERT INTO $this->$tablanev ($id)";
+                    if($result = $conn->querry($sql)){
+                        $this->id = $ud;
+                    }
+                }
+            
+    }
+
+    protected function get_id(){
+        return $this->id;        
+    }
+
+    protected function adminokListaja($conn){
+        $lista = array();
+        $sql = "SELECT id FROM $this->$tablanev";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            if($row = $result->fetch_assoc()){
+                while($row = $result->fetch_assoc()){
+                    $lista[] = $row['id'];
+                }
+
+            }
+        return $lista;
+        }
+        
+      
+    }
+
+
+    
+    
+    
+
+
+}/*
+
+$tanulo = new ulesrend;
+
+$tanulo->set_user(4, $conn);
+echo "Név: ";
+echo $tanulo->get_nev()."<br>Sor: ";
+echo $tanulo->get_sor()."<br>Oszlop: ";
+echo $tanulo->get_oszlop()."<br>Jelszo: ";
+echo $tanulo->get_jelszo()."<br>Felhasználónév: ";
+echo $tanulo->get_felhasznalo()."<br>";*/
+?>
