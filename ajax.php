79993f7c152e6381fef4282b753e836869a608bd
ArthabZerda <b.arthab1022@gmail.com>
2022-02-10 12:52:28 +0100

    bizt

--- ajax.php
+++ ajax.php
@@ -1,19 +1,35 @@
 <table>
 <?php
-require 'includes/db.inc.php';
-if(isset($_REQUEST['keres'])){
 
-    echo $sql = "SELECT `id`,`nev` ,`felhasznalonev` FROM `ulesrend` WHERE `nev` LIKE '%".$_REQUEST['keres']."%' ";
+require 'includes/db.inc.php';
+
+if(isset($_REQUEST['keres'])) {
    
-    if(!$result = $conn -> query($sql)) echo $conn->error;
-    if($result ->num_rows >0){
-        if($row = $result ->fetch_assoc()){
+    $sql = 'SELECT `id`,`nev`,`felhasznalonev` FROM ulesrend WHERE nev LIKE ?';
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param('s', $like); 
+    
+    $like = $_REQUEST['keres'].'%';
+    
+    $stmt->execute();
+
+    if($result = $stmt->get_result()) {
+
+        if ($result->num_rows > 0) {
+            
             while($row = $result->fetch_assoc()) {
                 echo "<tr><td>".$row['id']."</td><td>".$row['nev']."</td> <td>".$row['felhasznalonev']."</td>  </tr>";
             }
         }
-    }
+             
+    } 
 
 }
 
+
+
 ?><table>
+
+    
